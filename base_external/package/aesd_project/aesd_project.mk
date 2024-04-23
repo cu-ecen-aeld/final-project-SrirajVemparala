1366357d02a48c9913fc45c3dc34961ed2691d3f
vsrlinux <vsrlinux@vsrlinux.myguest.virtualbox.org>
2024-04-22 21:14:18 -0600

    socketchnages

--- base_external/package/aesd_project/aesd_project.mk
+++ base_external/package/aesd_project/aesd_project.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your aesd project git contents
-AESD_PROJECT_VERSION = 099c979647dc4b8373e380b68d21188b00e48e5e
+AESD_PROJECT_VERSION = 615464e7da8609136f1a88da28e946c530a0ea28
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -16,11 +16,13 @@ AESD_PROJECT_GIT_SUBMODULES = YES
 
 define AESD_PROJECT_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/MLX_code all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/socket all
 endef
 
 
 define AESD_PROJECT_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/MLX_code/* $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/socket/* $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
