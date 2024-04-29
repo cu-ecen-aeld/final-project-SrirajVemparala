a0431bbc5f4ef36205f12e9126abaf8eb7db5e98
vsrlinux <vsrlinux@vsrlinux.myguest.virtualbox.org>
2024-04-29 01:04:15 -0600

    Added_modprobe_changes

--- base_external/package/aesd_project/aesd_project.mk
+++ base_external/package/aesd_project/aesd_project.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your aesd project git contents
-AESD_PROJECT_VERSION = ff51460c41551c8e85df531e1910293f47d5da27
+AESD_PROJECT_VERSION = 235d36d7614aa28f18060360f8c81c72302f6b22
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -21,6 +21,7 @@ endef
 define AESD_PROJECT_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/MLX_code/* $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/socket/* $(TARGET_DIR)/usr/bin
+	#$(INSTALL) -m 0755 $(@D)/aesd-lcd/server-start-stop.sh $(TARGET_DIR)/etc/init.d/S99aesdserver
 endef
 
 $(eval $(generic-package))
